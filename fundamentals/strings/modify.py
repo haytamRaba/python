8dfa4de0d34df0bb3bc333916c54618a59caf31d
haytam <haytamraba11@gmail.com>
2025-03-09 02:36:13 +0000

    add all

---
+++ fundamentals/strings/modify.py
@@ -0,0 +1,15 @@
+
+w="  haytam Raba"
+print(w.upper())
+print(w)
+w=w.upper()
+print(w)
+print(w.lower())
+
+### strip() : removes any whitespace from beginning or the end
+print(w.strip())
+### replace 
+print(w.replace("A", "*"))
+## split()
+print(w.split(" ")) # ['', '', 'HAYTAM', 'RABA']
+
---
+++ fundamentals/strings/slicing.py
@@ -0,0 +1,8 @@
+# Get the characters from 
+# position 2 to position 5:
+word = "012345"
+print(word[2:5])
+# slice from the start
+print(word[:5])
+# slice to the end
+print(word[1:]) 
